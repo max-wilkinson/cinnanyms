b81610eb991b81ac71c2bfeb7e0cecbc74c7b556
Max Wilkinson <mawilkin@umich.edu>
2019-06-14 14:54:14 -0400

    Basic setup commit
    Does not even have the right name. lol

---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "keyboard-fu" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'extension.helloWorld',
+    function() {
+      // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor === undefined) {
+        vscode.window.showWarningMessage(
+          'This command requires a file to be open in the editor'
+        );
+        return;
+      }
+
+      const text = editor.document.getText(editor.selection);
+
+      if (isNullOrEmpty(text)) {
+        vscode.window.showWarningMessage(
+          'Please highlight some text to define'
+        );
+      }
+
+      vscode.window.showInformationMessage(text);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function isNullOrEmpty(input: any) {
+  return input === undefined || input === null || input === '';
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
